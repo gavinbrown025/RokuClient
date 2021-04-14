db28692ca74e2430df6a8cf9cbef33bf611b4d17
Gavin Brown <55294040+gavinbrown025@users.noreply.github.com>
2021-04-13 22:24:15 -0400

    selected movie details page

--- public/scripts/components/TheDetailsComponent.js
+++ public/scripts/components/TheDetailsComponent.js
@@ -2,16 +2,18 @@ import TheHeader from './TheHeaderComponent.js';
 export default {
     name: 'MovieDetailsComponent',
 
-    props: ['currentmovie'],
-
     data() {
         return ({
-            selectedMovie:{}
+            selectedMovie:{},
+            volume: 1,
         })
     },
 
     created() {
-         fetch(`api/movies/${this.currentmovie}`)
+
+        let pushedMovie = localStorage.getItem('selectedMovie');
+
+         fetch(`api/movies/${pushedMovie}`)
             .then(res => res.json())
             .then(data => {
                 this.selectedMovie = data;
@@ -20,24 +22,59 @@ export default {
     },
 
     template: `
-        <section>
-            <theheader @logout="$emit('logout')" />
-            <section class="movie-details-con">
-                <div class="preview-con">
-                    <video :src="'videos/'+currentmovie.movies_trailer"></video>
-                    <a class="button">PLAY MOVIE</a>
-                    <img src="" alt="" class="mute-btn">
-                </div>
-                <div class="moviedetails">
-                    <h1>{{currentmovie.movies_title}}</h1>
-                    rating | year | runtime
-                    Description |
+        <div>
+            <section class="details-con">
+                <theheader @logout="$emit('logout')" />
 
-                    cast | genres | studio | director
+                <div class="details-video-con">
+                    <video autoplay loop ref="preiviewPlayer" :src="'videos/'+selectedMovie.movies_trailer"></video>
+                    <img v-if="volume == 0" src="images/mute.svg" @click="mute" class="mute-btn">
+                    <img v-else src="images/sound.svg" @click="mute" class="mute-btn">
+                </div>
+
+                <div class="details-info-con">
+                    <div class="details-play">
+                        <h2>{{selectedMovie.movies_title}}</h2>
+                        <a class="button">PLAY MOVIE</a>
+                    </div>
+
+                    <div class="details-info">
+                        <p class="details-info-main">
+                            <span>{{selectedMovie.movies_urating}} Liked</span>
+                            <span>{{selectedMovie.movies_year}}</span>
+                            <span>{{selectedMovie.movies_arating}}</span>
+                            <span>{{selectedMovie.movies_runtime}}</span>
+                        </p>
+                        <p class="details-info-desc">
+                            {{selectedMovie.movies_storyline}}
+                        </p>
+                        <p>Genres: <span v-for="genre in selectedMovie.movies_genre">{{genre}}, </span></p>
+                    </div>
+
+                    <div class="details-info-meta">
+                        <p>Cast:<br> <span v-for="actor in selectedMovie.movies_cast">{{actor}}, </span></p>
+                        <p>Director: <span>{{selectedMovie.movies_director}}</span></p>
+                        <p>Studio: <span>{{selectedMovie.movies_studio}}</span></p>
+                    </div>
                 </div>
             </section>
-        </section>
+        </div>
     `,
+
+    mounted() {
+        this.$refs.preiviewPlayer.volume = this.volume;
+    },
+
+    methods:{
+        mute(){
+            if(this.volume > 0){
+                this.volume = 0;
+            } else {
+                this.volume = 1;
+            }
+            this.$refs.preiviewPlayer.volume = this.volume;
+        }
+    },
     components:{
         theheader: TheHeader,
     }
